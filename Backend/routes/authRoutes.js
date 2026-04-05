e673f40be5def567819a35508431a6fdc9e708e0
its1om1dhage <dhagesoham513@gmail.com>
2026-04-05 12:59:39 +0530

    Clear

--- Backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-// Login endpoint
-router.post("/login", authController.login);
-
-module.exports = router;
